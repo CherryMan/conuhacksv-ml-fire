7b1efc834e656f1065617af6ed61e3c72d6af921
Sheheryar Parvaz <skipper308@hotmail.ca>
2020-01-26 07:59:06 -0500

    Add indicators to map

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask, render_template
+import os
+import json
+from functools import lru_cache
+from flask import Flask, render_template, g
+import pandas as pd
 
 app = Flask(__name__)
 
+ENV = {
+    'MAPBOX_TOKEN': os.environ['MAPBOX_TOKEN'],
+}
+
+
+### Utils
+@lru_cache
+def load_data(path):
+    df = pd.read_csv(path)
+    df[['acq_time']] = df[['acq_time']].applymap(lambda x: f'{x:04}')
+
+    r = {}
+    for x in df.to_dict('records'):
+        k = x['acq_date']
+        del x['acq_date']
+
+        r.setdefault(k, [])
+        r[k] += [x]
+
+    return r
+
+
+### Routes
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('index.html', env=ENV)
+
+@app.route('/api/frames')
+def api_frames():
+    data = load_data('data/fire_nrt_M6_100853.csv')
+    return {'data': data}
--- static/index.js
+++ static/index.js
@@ -1,14 +1,30 @@
+const getData = () =>
+  fetch("/api/frames", { method: "GET" })
+    .then(r => r.json())
+    .then(r => r.data);
+
 function main() {
   let map = L.map("map").setView([1, 1], 3);
 
   L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
     {
-      attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      attribution: `Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, \
+         <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, \
+         Imagery © <a href="https://www.mapbox.com/">Mapbox</a>`,
       maxZoom: 18,
       id: "mapbox/dark-v10",
       accessToken: ENV.MAPBOX_TOKEN
     }
   ).addTo(map);
+
+  getData().then(r => {
+    for (const [_, v] of Object.entries(r)) {
+      for (const d of v) {
+        console.log(d)
+        L.circle([d.latitude, d.longitude], {radius: .01 * d.brightness})
+          .addTo(map);
+      }
+    }
+  });
 }
